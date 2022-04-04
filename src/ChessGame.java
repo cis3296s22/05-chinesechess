6a494b7601deff3f7aa9720f5b0772decccec47d
Athena Evans <70277494+APersonTakingClasses@users.noreply.github.com>
2022-04-04 14:25:38 -0400

    adding actionlistener

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -31,6 +31,13 @@ public class ChessGame {
         //add buttons
         f.add(newStartGame);
         f.add(ExitGame);
+        
+        //new game
+        newStartGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+            }
+        });
 
         f.setVisible(true);
     }
