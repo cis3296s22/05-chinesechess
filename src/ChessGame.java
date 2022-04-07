5bab295f933d73392f35c0b21daf24c8924bda8d
APersonTakingClasses <70277494+APersonTakingClasses@users.noreply.github.com>
2022-04-07 12:24:50 -0400

    Athena started commenting everything and didn't realize Jim was building a board as well

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -6,39 +6,48 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ChessGame {
+    //global variables
+    private static JFrame f;
+
     public static void main(String[] args) {
-        JFrame f = new JFrame("Chinese Chess Game");//title
-
-        f.setSize(800,800);
-        f.setLocationRelativeTo(null);
-
-        //set background
-        f.setLayout(new BorderLayout());
-        f.setContentPane(new JLabel(new ImageIcon("image/background.jpg")));
-        f.setLayout(new FlowLayout());
-
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        final Canvas c=  new Canvas();
-        f.add(c);
+        // Frame Creation Things vv
+        f = new JFrame("Chinese Chess Game");//title
+        f.setSize(800,800); // window size
+        f.setLocationRelativeTo(null); // null == center of screen
+        f.setLayout(new BorderLayout()); // boarder on frame
+        f.setContentPane(new JLabel(new ImageIcon("image/background.jpg"))); //set background
+        f.setLayout(new FlowLayout()); // how things sit on the frame
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit button top right
+        final Canvas c=  new Canvas(); // canvas is what the user interacts with
+        f.add(c); // add canvas (interaction) to the frame
 
         //create buttons
         JButton newStartGame = new JButton("New Game");
         JButton ExitGame = new JButton("Exit");
-
-
-
-        //add buttons
+        //add buttons to frame
         f.add(newStartGame);
         f.add(ExitGame);
-        
-        //new game
+
+        // exitGame Listener
+        ExitGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int j=JOptionPane.showConfirmDialog(null,"Are you sure? ","Exit",JOptionPane.YES_OPTION,JOptionPane.QUESTION_MESSAGE);
+                if(j==JOptionPane.YES_OPTION){
+                    System.exit(0);
+                }
+            }
+        });
+        // newStartGame Listener
         newStartGame.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
+                // make game?
+
+
             }
         });
 
-        f.setVisible(true);
+        f.setVisible(true); // make frame active
     }
 }
